25e4c83270770d14d75aa7c145cbfff32f37b557
ivanyeung43 <ivany43@nycstudents.net>
2026-03-24 13:58:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 ## Open the JSON file of pokemon data
 pokedex = open("./pokedex.json", encoding="utf8")
-
+moves = open("./moves.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 
 
@@ -16,7 +16,7 @@ pokedex = open("./pokedex.json", encoding="utf8")
 # For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
 
 data = json.load(pokedex)
-
+move_data = json.load(moves)
 
 """ def find_name(id):
     lang_choice = input("What language do you prefer? English, Japanese, Chinese, or French")
@@ -43,15 +43,16 @@ find_name(data[-1]["id"]) """
             print(data[count]["name"])
 find_type(data[-1]["id"]) """
 
-def poke_search(search_list):
+""" def poke_search(search_list):
     found_poke = []
     found = 0
     for count in range(data[-1]["id"]):
-        if search_list in data[count]["name"]["enlgish"]:
-            found_poke.append(data[count]["name"]["enlgish"])
+        if search_list in data[count]["name"]["english"]:
+            found_poke.append(data[count]["name"]["english"])
             found += 1
     if found > 0:
         print(found_poke)
     if found == 0:
         print("No pokemon found")
-poke_search("Char")
+poke_search("Char") """
+
